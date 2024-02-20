fa1ad80ce2041d9c12cc6e3ec2ea4bc6797c6b44
Eggward12 <cristian.sanchez.casd@gmail.com>
2024-02-19 22:13:55 -0500

    popupfix

--- src/components/popupBusqueda.jsx
+++ src/components/popupBusqueda.jsx
@@ -1,7 +1,7 @@
 export const PopupSearch = ({ datos }) => {
     return (
       <>
-        <ul className="absolute text-left top-[10%] left-[29%] w-[30%] bg-slate-800 py-3 rounded-md z-20">
+        <ul className="absolute text-left top-[10%] left-[50.5%] w-[30%] bg-slate-800 py-3 rounded-md z-20">
           {datos.map((element) => {
             return <li className="ps-4 py-1 hover:bg-slate-900" key={element.id}>{element.login}</li>;
           })}
